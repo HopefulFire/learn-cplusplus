1e07fa746c5319d59b52b4c662691ae1cccc0f32
Jade Lynn Masker <hopefulfire@gmail.com>
2020-05-11 18:36:13 -0400

    added official solution to favorite number challenge

--- projects/zzwmyfavoritenumber/main.cpp
+++ projects/zzwmyfavoritenumber/main.cpp
@@ -6,7 +6,5 @@ int main()
 	std::cout << "Enter your favorite number between 1 and 100: ";
 	std::cin >> number;
 	std::cout << "Amazing!! That's my favorite number too!\n";
-	std::cout << "No really!! ";
-	std::cout << number;
-	std::cout << " is my favorite number!\n";
+	std::cout << "No really!! " << number << " is my favorite number!\n";
 }
