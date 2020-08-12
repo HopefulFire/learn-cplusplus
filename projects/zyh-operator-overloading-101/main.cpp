08768cd3eec273822446d11f34ea0795486cd7f5
Jade Lynn Masker <hopefulfire@gmail.com>
2020-08-12 19:42:13 -0400

    remove solution in main

--- projects/zyh-operator-overloading-101/main.cpp
+++ projects/zyh-operator-overloading-101/main.cpp
@@ -2,13 +2,5 @@
 #include "String.h"
 
 int main() {
-	String larry{"Larry"};
-	String moe{"Moe"};
-	String curly;
-	std::cout << "Enter the third stooge's first name: ";
-	std::cin >> curly;
-
-	std::cout << "The three stooges are " << larry << ", " << moe << ", and " << curly << std::endl;
-
 	return 0;
 }
